8bad5d3fe783eeb7ba8d04c706b38af1747ea06c
Simon <jorstedtsimon@gmail.com>
2024-10-23 08:20:32 +0200

    Added unit tests for bubble and bogo
    Used the same tests for both algorithms.

---
+++ tests/testthat/test-bubble_sort.R
@@ -0,0 +1,24 @@
+### Set which sorting algorithm to test
+sorting_algorithm <- bubble_sort
+
+### Test that the correct output is returned
+test_that("Test that correct output is returned", {
+  expect_equal(sorting_algorithm(c(3,5,1,2), ascending=T), c(1,2,3,5))
+})
+
+test_that("Test that correct output is returned", {
+  expect_equal(sorting_algorithm(c(3,5,1,2), ascending=0), c(5,3,2,1))
+})
+
+test_that("Test that correct output is returned", {
+  expect_equal(sorting_algorithm(c(1,"A",3,2, "B")), c("1", "2", "3", "A", "B"))
+})
+
+### Test that errors are thrown for incorrect arguments
+test_that("Errors are thrown", {
+  expect_error(sorting_algorithm(c(1), ascending=3))
+})
+
+test_that("Errors are thrown", {
+  expect_error(sorting_algorithm(list(c(1,2,3), "A")))
+})
